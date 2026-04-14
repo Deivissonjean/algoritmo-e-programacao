e4a8bfccc73b72f31b310603631e214fa353ca80
deivissonjean <deivissonjeanoficial@gmail.com>
2026-04-14 19:57:06 -0300

    atualizacao

--- java inicio 07-04-2026/java 07 04/src/ex6maiorvalor.java
+++ java inicio 07-04-2026/java 07 04/src/ex6maiorvalor.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ex6maiorvalor
 {
-    static void main(String[] args)
+    public static void main(String[] args)
     {
 
         Scanner ler = new Scanner(System.in);
